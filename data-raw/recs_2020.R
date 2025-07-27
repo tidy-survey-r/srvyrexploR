5a3456ce38b475e3a665dd340e6c0753cdcb75ae
Stephanie Zimmer <stephaniezimmer@gmail.com>
2025-07-27 14:13:41 -0400

    Initial CHIS data (#14)
    * Initial CHIS data
    
    * Simplify labels
    
    * styler applied to package

--- data-raw/recs_2020.R
+++ data-raw/recs_2020.R
@@ -3,17 +3,17 @@ knitr::opts_chunk$set(echo = TRUE)
 
 
 ## --------------------------------------------------------------------------------------------------------------------------------------------------------------------
-library(tidyverse) #data manipulation
-library(haven) #data import
-library(tidylog) #informative logging messages
+library(tidyverse) # data manipulation
+library(haven) # data import
+library(tidylog) # informative logging messages
 library(osfr)
 
 
 ## --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 recs_file_osf_det <- osf_retrieve_node("https://osf.io/z5c3m/") %>%
-  osf_ls_files(path="RECS_2020", pattern="sas7bdat") %>%
+  osf_ls_files(path = "RECS_2020", pattern = "sas7bdat") %>%
   filter(str_detect(name, "v5")) %>%
-  osf_download(conflicts="overwrite", path=here::here("data-raw"))
+  osf_download(conflicts = "overwrite", path = here::here("data-raw"))
 
 recs_in <- haven::read_sas(pull(recs_file_osf_det, local_path))
 
@@ -32,18 +32,18 @@ unlink(pull(recs_file_osf_det, local_path))
 # BTUWD replaces WOODBTU
 # BRR weights are NWEIGHT
 
-zimp01 <- function(x){
+zimp01 <- function(x) {
   parse_factor(
     case_match(
       x,
       0 ~ "Not imputed",
       1 ~ "Imputed"
     ),
-    levels=c("Not imputed", "Imputed"),
+    levels = c("Not imputed", "Imputed"),
     include_na = FALSE
   )
 }
-zimp012 <- function(x){
+zimp012 <- function(x) {
   parse_factor(
     case_match(
       x,
@@ -51,87 +51,96 @@ zimp012 <- function(x){
       1 ~ "Imputed",
       -2 ~ "Not applicable"
     ),
-    levels=c("Not imputed", "Imputed", "Not applicable"),
+    levels = c("Not imputed", "Imputed", "Not applicable"),
     include_na = FALSE
   )
 }
 
 
 recs <- recs_in %>%
-  select(DOEID, REGIONC, DIVISION, STATE_FIPS, state_postal, state_name, UATYP10, TYPEHUQ, YEARMADERANGE, HEATHOME, HEATCNTL, TEMPHOME, TEMPGONE, TEMPNITE, AIRCOND, COOLCNTL, TEMPHOMEAC, TEMPGONEAC, TEMPNITEAC, TOTCSQFT, TOTHSQFT, TOTSQFT_EN, NWEIGHT, starts_with("NWEIGHT"), CDD30YR=CDD30YR_PUB, CDD65, BA_climate, IECC_climate_code, HDD30YR=HDD30YR_PUB, HDD65,
-         BTUEL, DOLLAREL, BTUNG, DOLLARNG, BTULP, DOLLARLP, BTUFO, DOLLARFO, TOTALBTU, TOTALDOL, BTUWOOD=BTUWD) %>%
+  select(DOEID, REGIONC, DIVISION, STATE_FIPS, state_postal, state_name, UATYP10, TYPEHUQ, YEARMADERANGE, HEATHOME, HEATCNTL, TEMPHOME, TEMPGONE, TEMPNITE, AIRCOND, COOLCNTL, TEMPHOMEAC, TEMPGONEAC, TEMPNITEAC, TOTCSQFT, TOTHSQFT, TOTSQFT_EN, NWEIGHT, starts_with("NWEIGHT"),
+    CDD30YR = CDD30YR_PUB, CDD65, BA_climate, IECC_climate_code, HDD30YR = HDD30YR_PUB, HDD65,
+    BTUEL, DOLLAREL, BTUNG, DOLLARNG, BTULP, DOLLARLP, BTUFO, DOLLARFO, TOTALBTU, TOTALDOL, BTUWOOD = BTUWD
+  ) %>%
   mutate(
-    Region=parse_factor(
+    Region = parse_factor(
       str_to_title(REGIONC),
-      levels=c("Northeast", "Midwest", "South", "West")),
-    Division=parse_factor(
-      DIVISION, levels=c("New England", "Middle Atlantic", "East North Central", "West North Central", "South Atlantic", "East South Central", "West South Central", "Mountain North", "Mountain South", "Pacific")),
-    Urbanicity=parse_factor(
-      case_when(
-        UATYP10=="U"~"Urban Area",
-        UATYP10=="C"~"Urban Cluster",
-        UATYP10=="R"~"Rural"
-      ),
-      levels=c("Urban Area", "Urban Cluster", "Rural")
+      levels = c("Northeast", "Midwest", "South", "West")
     ),
-    HousingUnitType=parse_factor(
+    Division = parse_factor(
+      DIVISION,
+      levels = c("New England", "Middle Atlantic", "East North Central", "West North Central", "South Atlantic", "East South Central", "West South Central", "Mountain North", "Mountain South", "Pacific")
+    ),
+    Urbanicity = parse_factor(
       case_when(
-        TYPEHUQ==1~"Mobile home",
-        TYPEHUQ==2~"Single-family detached",
-        TYPEHUQ==3~"Single-family attached",
-        TYPEHUQ==4~"Apartment: 2-4 Units",
-        TYPEHUQ==5~"Apartment: 5 or more units",
-      ), levels=c("Mobile home", "Single-family detached", "Single-family attached", "Apartment: 2-4 Units", "Apartment: 5 or more units")),
-    YearMade=parse_factor(
-      case_when(
-        YEARMADERANGE==1~"Before 1950",
-        YEARMADERANGE==2~"1950-1959",
-        YEARMADERANGE==3~"1960-1969",
-        YEARMADERANGE==4~"1970-1979",
-        YEARMADERANGE==5~"1980-1989",
-        YEARMADERANGE==6~"1990-1999",
-        YEARMADERANGE==7~"2000-2009",
-        YEARMADERANGE==8~"2010-2015",
-        YEARMADERANGE==9~"2016-2020"
+        UATYP10 == "U" ~ "Urban Area",
+        UATYP10 == "C" ~ "Urban Cluster",
+        UATYP10 == "R" ~ "Rural"
       ),
-      levels=c("Before 1950", "1950-1959", "1960-1969", "1970-1979", "1980-1989", "1990-1999", "2000-2009", "2010-2015", "2016-2020"),
+      levels = c("Urban Area", "Urban Cluster", "Rural")
+    ),
+    HousingUnitType = parse_factor(
+      case_when(
+        TYPEHUQ == 1 ~ "Mobile home",
+        TYPEHUQ == 2 ~ "Single-family detached",
+        TYPEHUQ == 3 ~ "Single-family attached",
+        TYPEHUQ == 4 ~ "Apartment: 2-4 Units",
+        TYPEHUQ == 5 ~ "Apartment: 5 or more units",
+      ),
+      levels = c("Mobile home", "Single-family detached", "Single-family attached", "Apartment: 2-4 Units", "Apartment: 5 or more units")
+    ),
+    YearMade = parse_factor(
+      case_when(
+        YEARMADERANGE == 1 ~ "Before 1950",
+        YEARMADERANGE == 2 ~ "1950-1959",
+        YEARMADERANGE == 3 ~ "1960-1969",
+        YEARMADERANGE == 4 ~ "1970-1979",
+        YEARMADERANGE == 5 ~ "1980-1989",
+        YEARMADERANGE == 6 ~ "1990-1999",
+        YEARMADERANGE == 7 ~ "2000-2009",
+        YEARMADERANGE == 8 ~ "2010-2015",
+        YEARMADERANGE == 9 ~ "2016-2020"
+      ),
+      levels = c("Before 1950", "1950-1959", "1960-1969", "1970-1979", "1980-1989", "1990-1999", "2000-2009", "2010-2015", "2016-2020"),
       ordered = TRUE
     ),
-    SpaceHeatingUsed=as.logical(HEATHOME),
-    HeatingBehavior=parse_factor(
+    SpaceHeatingUsed = as.logical(HEATHOME),
+    HeatingBehavior = parse_factor(
       case_when(
-        HEATCNTL==1~"Set one temp and leave it",
-        HEATCNTL==2~"Manually adjust at night/no one home",
-        HEATCNTL==3~"Programmable or smart thermostat automatically adjusts the temperature",
-        HEATCNTL==4~"Turn on or off as needed",
-        HEATCNTL==5~"No control",
-        HEATCNTL==99~"Other",
-        HEATCNTL==-2~NA_character_),
-      levels=c("Set one temp and leave it", "Manually adjust at night/no one home", "Programmable or smart thermostat automatically adjusts the temperature", "Turn on or off as needed", "No control", "Other"),
+        HEATCNTL == 1 ~ "Set one temp and leave it",
+        HEATCNTL == 2 ~ "Manually adjust at night/no one home",
+        HEATCNTL == 3 ~ "Programmable or smart thermostat automatically adjusts the temperature",
+        HEATCNTL == 4 ~ "Turn on or off as needed",
+        HEATCNTL == 5 ~ "No control",
+        HEATCNTL == 99 ~ "Other",
+        HEATCNTL == -2 ~ NA_character_
+      ),
+      levels = c("Set one temp and leave it", "Manually adjust at night/no one home", "Programmable or smart thermostat automatically adjusts the temperature", "Turn on or off as needed", "No control", "Other"),
       include_na = FALSE
     ),
-    WinterTempDay=if_else(TEMPHOME>0, TEMPHOME, NA_real_),
-    WinterTempAway=if_else(TEMPGONE>0, TEMPGONE, NA_real_),
-    WinterTempNight=if_else(TEMPNITE>0, TEMPNITE, NA_real_),
-    ACUsed=as.logical(AIRCOND),
-    ACBehavior=parse_factor(
+    WinterTempDay = if_else(TEMPHOME > 0, TEMPHOME, NA_real_),
+    WinterTempAway = if_else(TEMPGONE > 0, TEMPGONE, NA_real_),
+    WinterTempNight = if_else(TEMPNITE > 0, TEMPNITE, NA_real_),
+    ACUsed = as.logical(AIRCOND),
+    ACBehavior = parse_factor(
       case_when(
-        COOLCNTL==1~"Set one temp and leave it",
-        COOLCNTL==2~"Manually adjust at night/no one home",
-        COOLCNTL==3~"Programmable or smart thermostat automatically adjusts the temperature",
-        COOLCNTL==4~"Turn on or off as needed",
-        COOLCNTL==5~"No control",
-        COOLCNTL==99~"Other",
-        COOLCNTL==-2~NA_character_),
-      levels=c("Set one temp and leave it", "Manually adjust at night/no one home", "Programmable or smart thermostat automatically adjusts the temperature", "Turn on or off as needed", "No control", "Other"),
+        COOLCNTL == 1 ~ "Set one temp and leave it",
+        COOLCNTL == 2 ~ "Manually adjust at night/no one home",
+        COOLCNTL == 3 ~ "Programmable or smart thermostat automatically adjusts the temperature",
+        COOLCNTL == 4 ~ "Turn on or off as needed",
+        COOLCNTL == 5 ~ "No control",
+        COOLCNTL == 99 ~ "Other",
+        COOLCNTL == -2 ~ NA_character_
+      ),
+      levels = c("Set one temp and leave it", "Manually adjust at night/no one home", "Programmable or smart thermostat automatically adjusts the temperature", "Turn on or off as needed", "No control", "Other"),
       include_na = FALSE
     ),
-    SummerTempDay=if_else(TEMPHOMEAC>0, TEMPHOMEAC, NA_real_),
-    SummerTempAway=if_else(TEMPGONEAC>0, TEMPGONEAC, NA_real_),
-    SummerTempNight=if_else(TEMPNITEAC>0, TEMPNITEAC, NA_real_),
-    ClimateRegion_BA=parse_factor(BA_climate),
-    state_name=factor(state_name),
-    state_postal=fct_reorder(state_postal, as.numeric(state_name))
+    SummerTempDay = if_else(TEMPHOMEAC > 0, TEMPHOMEAC, NA_real_),
+    SummerTempAway = if_else(TEMPGONEAC > 0, TEMPGONEAC, NA_real_),
+    SummerTempNight = if_else(TEMPNITEAC > 0, TEMPNITEAC, NA_real_),
+    ClimateRegion_BA = parse_factor(BA_climate),
+    state_name = factor(state_name),
+    state_postal = fct_reorder(state_postal, as.numeric(state_name))
   )
 
 
@@ -149,19 +158,23 @@ recs %>% count(HeatingBehavior, HEATCNTL)
 recs %>% count(ACUsed, AIRCOND)
 recs %>% count(ACBehavior, COOLCNTL)
 recs %>% count(ClimateRegion_BA, BA_climate)
-recs %>% count(state_postal, state_name, STATE_FIPS) %>% print(n=51)
+recs %>%
+  count(state_postal, state_name, STATE_FIPS) %>%
+  print(n = 51)
 
 recs_out <- recs %>%
-  select(DOEID, starts_with("NWEIGHT"),
-         REGIONC, Region, Division, starts_with("state"), Urbanicity,
-         HousingUnitType, YearMade, SpaceHeatingUsed, HeatingBehavior,
-         WinterTempDay, WinterTempAway, WinterTempNight, ACUsed,
-         ACBehavior, SummerTempDay, SummerTempAway, SummerTempNight,
-         TOTCSQFT, TOTHSQFT, TOTSQFT_EN,
-         CDD30YR, CDD65, ClimateRegion_BA,
-         HDD30YR, HDD65, BTUEL,
-         DOLLAREL, BTUNG, DOLLARNG, BTULP, DOLLARLP, BTUFO, DOLLARFO,
-         TOTALBTU, TOTALDOL, BTUWOOD)
+  select(
+    DOEID, starts_with("NWEIGHT"),
+    REGIONC, Region, Division, starts_with("state"), Urbanicity,
+    HousingUnitType, YearMade, SpaceHeatingUsed, HeatingBehavior,
+    WinterTempDay, WinterTempAway, WinterTempNight, ACUsed,
+    ACBehavior, SummerTempDay, SummerTempAway, SummerTempNight,
+    TOTCSQFT, TOTHSQFT, TOTSQFT_EN,
+    CDD30YR, CDD65, ClimateRegion_BA,
+    HDD30YR, HDD65, BTUEL,
+    DOLLAREL, BTUNG, DOLLARNG, BTULP, DOLLARLP, BTUFO, DOLLARFO,
+    TOTALBTU, TOTALDOL, BTUWOOD
+  )
 
 
 
@@ -170,15 +183,17 @@ for (var in colnames(recs_out)) {
   recs_out[[var]] <- zap_formats(recs_out[[var]])
 }
 
-cb_in <- readxl::read_xlsx(here::here("data-raw", "RECS 2020 Codebook Questions.xlsx"), skip=1)
+cb_in <- readxl::read_xlsx(here::here("data-raw", "RECS 2020 Codebook Questions.xlsx"), skip = 1)
 
 cb_ord <- cb_in %>%
   mutate(
-    Order=row_number(),
-    Section=if_else(Section=="End-use Model", "CONSUMPTION AND EXPENDITURE", Section),
-    Section=fct_reorder(Section, Order, min))
+    Order = row_number(),
+    Section = if_else(Section == "End-use Model", "CONSUMPTION AND EXPENDITURE", Section),
+    Section = fct_reorder(Section, Order, min)
+  )
 
-cb_slim <- cb_ord %>% select(Variable=BookDerived, `Description and Labels`, Question, Section, Order) %>%
+cb_slim <- cb_ord %>%
+  select(Variable = BookDerived, `Description and Labels`, Question, Section, Order) %>%
   filter(!is.na(Variable)) %>%
   bind_rows(select(cb_ord, Variable, `Description and Labels`, Question, Section, Order)) %>%
   arrange(Section, Order)
@@ -194,7 +209,7 @@ ncol(recs_out)
 recs_ord <- recs_out %>% select(all_of(pull(cb_vars, Variable)))
 
 for (var in pull(cb_vars, Variable)) {
-  vi <- cb_vars %>% filter(Variable==var)
+  vi <- cb_vars %>% filter(Variable == var)
   attr(recs_ord[[deparse(as.name(var))]], "label") <- pull(vi, `Description and Labels`)
   attr(recs_ord[[deparse(as.name(var))]], "Section") <- pull(vi, Section) %>% as.character()
   if (!is.na(pull(vi, Question))) attr(recs_ord[[deparse(as.name(var))]], "Question") <- pull(vi, Question)
